7c3444b1ae0404327ccec3c7c31472d3276e27fb
Matt110110 <matruprasadchand@gmail.com>
2017-08-16 20:18:36 +0530

    Button fixed

--- script.js
+++ script.js
@@ -1,30 +1,4 @@
 $("Document").ready(function () {
-    $('[data-toggle="tooltip"]').tooltip(); 
-    
-    // Add smooth scrolling to all links in navbar + footer link
-    $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
-  
-      // Make sure this.hash has a value before overriding default behavior
-      if (this.hash !== "") {
-  
-        // Prevent default anchor click behavior
-        event.preventDefault();
-  
-        // Store hash
-        var hash = this.hash;
-  
-        // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 900, function(){
-     
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-        });
-      } // End if
-    });
-
     $("#linkedIn").click(function () {
         window.location="https://www.linkedin.com/in/matruprasad-chand-137a35133/";
     });
@@ -40,6 +14,7 @@ $("Document").ready(function () {
     $("#github").click(function () {
         window.location="https://github.com/Matt110110";
     });
-    
-
+});
+document.getElementById("But").addEventListener("click", function () {
+   document.querySelector(".navbar-collapse").classList.toggle('in');
 });
